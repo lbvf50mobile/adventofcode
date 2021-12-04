8f42e847af045b2b3246aa33909f11e201d56555
Dmitry <lbvf50.mobile@gmail.com>
2021-12-04 23:46:39 +0700

    Day 4: Giant Squid. (in action)

--- 2021/04/2021_04.rb
+++ 2021/04/2021_04.rb
@@ -1,14 +1,56 @@
 require 'set'
 class Solution
   def start1(arr)
-    return "First 1\n"
+    ans = "First 1\n"
+    numbers = arr.shift
+    numbers = numbers.split(" ").map(&:to_i)
+    checked_numbers = []
+    desks = [] 
+    arr.each_slice(6){|x| desks.push Desk.new(x) }
+    winner = nil
+    numbers.each do |n|
+      checked_numbers.push(n)
+      winner = false
+      desks.each do |desk|
+        tmp = desk.check(n)
+        winner = tmp if tmp
+        break if winner
+      end
+      break if winner
+    end
+    raise "Cannot find winer" if ! winner
+    
+    return ans;
   end
   def start2(arr)
     return "Second 2\n"
   end
 end
 
-
+class Desk
+  # Accepts 6 lites. Fist is empty one.
+  def initialize(arr)
+    @raw = arr.join("\n")+"\n"
+    @winner = nil
+    arr.shift
+    numbers = arr.map{|x| x.split(" ").map(&:to_i)}
+    @lines = []
+    # Rows:
+    numbers.each{|x| @lines.push(Line.new(x))}
+    numbers.transpose.each{|x| @lines.push(Line.new(x))}
+  end
+  def check(number)
+    p number
+    @lines.each{|x| x.check(number)}
+    @winner = @lines.find{|x| x.win}
+  end
+  def inspect
+    ans = ""
+    ans += @raw
+    ans += @winner.inspect + "\n" if @winner
+    ans
+  end
+end
 # 3. `desk` have `lines` line contains `nums_set`, `marked` and `win` boolean. Consturctor accepts an array. Methods `check` and `win` and `result`. 
 class Line
   attr_reader :win
