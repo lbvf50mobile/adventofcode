fd8bd8eef66d25d3e32227cb965bd9ae1eecbaae
Dmitry <lbvf50.mobile@gmail.com>
2021-12-06 00:34:58 +0700

    Day 5: Hydrothermal Venture. (First answer. Thanks God\!)

--- 2021/05/2021_05.rb
+++ 2021/05/2021_05.rb
@@ -4,6 +4,7 @@ class Solution
     p "1================================"
     ans = "First 1\n"
     lines = convert_arr_to_lines(arr)
+    @lines = lines
     check = check_lines_correctnes(arr,lines)
     mes = "Check correct convertions: #{check.inspect}. Sizes lines=#{lines.size}, arr=#{arr.size}\n"
     p mes
@@ -12,24 +13,13 @@ class Solution
     maxx,maxy = find_maxx_maxy(lines)
     ans += "maxx=#{maxx}, maxy=#{maxy}\n"
     @matrix = Array.new(maxy+1).map{ Array.new(maxx+1,0)}
-    ans += string_lines(lines)
-    horizontal = select_horizontal(lines)
-    mes = "Horizontal size: #{horizontal.size}, #{horizontal[0].inspect}\n"
+    increase_v_and_h
+    answer = @matrix.flatten.count{|x| x >= 2}
+    mes = "Answer is: #{answer}\n"
     p mes
     ans += mes
-    ans += string_lines(horizontal)
-    draw_horizontal(horizontal)
-    ans += string_matrix
-    vertical = select_vertical(lines)
-    mes = "Vertical size: #{vertical.size}, #{vertical[0].inspect}\n"
-    p mes
-    ans += mes
-    draw_vertical(vertical)
-    ans += string_lines(vertical)
-    ans += string_matrix
-    answer = count_cells_bigger_then_1
-    p "Number of cells greater or equal to 2 is #{answer}"
-    ans = "Number of cells greater or equal to 2 is #{answer}\n"  + ans
+    ans += str_mtrx
+    
     return ans
   end
   def start2(arr)
@@ -53,56 +43,27 @@ class Solution
   def find_maxx_maxy(lines)
     maxx = (lines.map{|x| x[0][0]} + lines.map{|x| x[1][0]}).max
     maxy = (lines.map{|x| x[0][1]} + lines.map{|x| x[1][1]}).max
-
     [maxx,maxy]
   end
-  def select_horizontal(lines)
-    lines.select{|x| x[0][1] == x[1][1]}
-  end
-  def select_vertical(lines)
-    lines.select{|x| x[0][0] == x[1][0]}
-  end
-  def draw_horizontal(lines)
-    # p "I am draw horizontal and I draw #{lines.size} lines."
-    counter = 0
-    dots = 0
-    lines.each_with_index do |line,index|
-      raise "No horizontal." if line[0][1] != line[1][1]
-      y = line[0][1]
-      counter += 1
-      # p "Draw #{line.inspect} y = #{y} and #{[line[0][0],line[1][0]].inspect}."
-      min,max = [line[0][0],line[1][0]].minmax
-      (min..max).each do |x|
-        raise "No Y for y = #{y}, x = #{x} Horizontal" if ! @matrix[y]
-        raise "No X for y = #{y}, x = #{x} Horizontal" if ! @matrix[y][x]
-        @matrix[y][x] += 1
-        dots += 1
+  def increase_v_and_h
+    @lines.each do |l|
+      a,b = l[0], l[1]
+      if a[0] == b[0]
+        x = a[0]
+        min,max = [a[1],b[1]].minmax
+        (min..max).each{|y| @matrix[y][x] += 1 }
       end
-    end
-    # p "Was drawn #{counter} lines. And #{dots} cells."
-  end
-  def draw_vertical(lines)
-    # p "I am draw vertical and I draw #{lines.size} lines."
-    lines.each do |line|
-      x = line[0][0]
-      min,max = [line[0][1],line[1][1]].minmax
-      (min..max).each do |y|
-        @matrix[y][x] += 1
+      if a[1] == b[1]
+        y = a[1]
+        min,max = [a[0],b[0]].minmax
+        (min..max).each{|x| @matrix[y][x] += 1 }
       end
     end
   end
-  def count_cells_bigger_then_1
-    @matrix.flatten.select{|x| x > 1}.size
+  def str_mtrx
+    x = @matrix.map{|row| row.map(&:to_s).map{|x| ?0 == x ? ?. : x}.join(" ")}
+    x.join("\n")
   end
-  def string_lines(lines)
-    ans = ""
-    lines.each_with_index do |line,i|
-      ans += "#{i+1} " + line.inspect + "\n"
-    end
-    ans
-  end
-  def string_matrix
-    @matrix.map{|x| x.join(?,)}.join("\n") + "\n"
-  end
+
 end
 
