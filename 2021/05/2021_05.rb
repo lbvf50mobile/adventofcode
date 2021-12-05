fd468541acf4778a8599ce7f7e4bdceaa48ea4aa
Dmitry <lbvf50.mobile@gmail.com>
2021-12-05 23:51:23 +0700

    Day 5: Hydrothermal Venture (incorrect answer).

--- 2021/05/2021_05.rb
+++ 2021/05/2021_05.rb
@@ -6,6 +6,7 @@ class Solution
     maxx,maxy = find_maxx_maxy(lines)
     ans += "maxx=#{maxx}, maxy=#{maxy}\n"
     @matrix = Array.new(maxy+1).map{ Array.new(maxx+1,0)}
+    ans += string_lines(lines)
     horizontal = select_horizontal(lines)
     mes = "Horizontal size: #{horizontal.size}, #{horizontal[0].inspect}\n"
     p mes
@@ -14,11 +15,15 @@ class Solution
     draw_horizontal(horizontal)
     ans += string_matrix
     vertical = select_vertical(lines)
-    p vertical.size
+    mes = "Vertical size: #{vertical.size}, #{vertical[0].inspect}\n"
+    p mes
+    ans += mes
     draw_vertical(vertical)
+    ans += string_lines(vertical)
+    ans += string_matrix
     answer = count_cells_bigger_then_1
     p "Number of cells greater or equal to 2 is #{answer}"
-    ans += "Number of cells greater or equal to 2 is #{answer}" 
+    ans = "Number of cells greater or equal to 2 is #{answer}\n"  + ans
     return ans
   end
   def start2(arr)
@@ -31,13 +36,9 @@ class Solution
     }
   end
   def find_maxx_maxy(lines)
-    maxx, maxy = -1, -1
-    lines.each do |line|
-      maxx = line[0][0] if maxx < line[0][0]
-      maxx = line[1][0] if maxx < line[1][0]
-      maxy = line[0][1] if maxy < line[0][1]
-      maxy = line[1][1] if maxy < line[1][1]
-    end
+    maxx = (lines.map{|x| x[0][0]} + lines.map{|x| x[1][0]}).max
+    maxy = (lines.map{|x| x[0][1]} + lines.map{|x| x[1][1]}).max
+
     [maxx,maxy]
   end
   def select_horizontal(lines)
@@ -47,20 +48,30 @@ class Solution
     lines.select{|x| x[0][0] == x[1][0]}
   end
   def draw_horizontal(lines)
-    lines.each do |line|
+    # p "I am draw horizontal and I draw #{lines.size} lines."
+    counter = 0
+    dots = 0
+    lines.each_with_index do |line,index|
       raise "No horizontal." if line[0][1] != line[1][1]
       y = line[0][1]
-      (line[0][0]..line[1][0]).each do |x|
+      counter += 1
+      # p "Draw #{line.inspect} y = #{y} and #{[line[0][0],line[1][0]].inspect}."
+      min,max = [line[0][0],line[1][0]].minmax
+      (min..max).each do |x|
         raise "No Y for y = #{y}, x = #{x} Horizontal" if ! @matrix[y]
         raise "No X for y = #{y}, x = #{x} Horizontal" if ! @matrix[y][x]
         @matrix[y][x] += 1
+        dots += 1
       end
     end
+    # p "Was drawn #{counter} lines. And #{dots} cells."
   end
   def draw_vertical(lines)
+    # p "I am draw vertical and I draw #{lines.size} lines."
     lines.each do |line|
       x = line[0][0]
-      (line[0][1]..line[1][1]).each do |y|
+      min,max = [line[0][1],line[1][1]].minmax
+      (min..max).each do |y|
         @matrix[y][x] += 1
       end
     end
