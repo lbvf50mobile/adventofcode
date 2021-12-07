a663b0c9b946df3c9a1ca1c626449abd15780b5f
Dmitry <lbvf50.mobile@gmail.com>
2021-12-07 23:30:20 +0700

    Day 7: The Treachery of Whales. Second task is done.

--- 2021/07/2021_07.rb
+++ 2021/07/2021_07.rb
@@ -1,3 +1,7 @@
+# = = = = = = =
+# Accepted.
+# Thanks God!
+# = = = = = = =
 class Solution
   def start1(arr)
     @l = ""
@@ -36,6 +40,36 @@ class Solution
   end
   def start2(arr)
     @l = ""
+    @crabs, @positions = [],{}
+    get_crabs_and_positions arr
+    correct_input = check_crabs_converted_correctly arr
+    debug = false
+    l "Input parsed correctly: #{correct_input}"
+    l "Debuging: #{debug.inspect}"
+    l "Number of crabs: #{@crabs.size}"
+    l "Number of positions: #{@positions.keys.size}"
+    start_pos,end_pos = @positions.keys.minmax
+    l "From #{start_pos} til #{end_pos} there are #{end_pos - start_pos + 1} steps"
+    l "Crabs: #{@crabs.inspect}" if debug
+    l "Positions: #{@positions.inspect}" if debug
+    position_fuel_price = {} # Cost to move crabs to the position.
+    (start_pos..end_pos).each do |destination|
+      l "Start Move to #{destination}. ===================" if debug
+      position_fuel_price[destination] = 0
+      @positions.keys.each do |source|
+        pos_amount =  @positions[source]
+        way = (source - destination).abs
+        price =  0 == way ? 0 : ((way*(1+way))/2)*pos_amount
+        l "#{price}  #{price}  #{destination}-#{source} = #{way}"  if debug
+        position_fuel_price[destination] += price
+      end
+      l "#{position_fuel_price[destination]} :total" if debug
+      l "End Move to #{destination}. ===================\n\n" if debug
+    end
+    min = position_fuel_price.to_a.min_by{|x| x[1]}
+    l "Min: #{min.inspect}" if debug
+    l "Minumum cost is: #{min[1]} at the poistion #{min[0]}"
+    l_up "Answer: #{min[1]}"
     l_up "Second 2."
     return @l
   end
