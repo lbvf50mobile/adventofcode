c66590e83ee3e9a7ab85b4d5e86728d2ed94ab78
Dmitry <lbvf50.mobile@gmail.com>
2021-12-08 23:20:33 +0700

    Day 8: Seven Segment Search. Part one. Thanks God!

---
+++ 2021/08/2021_08.rb
@@ -0,0 +1,45 @@
+class Solution
+  def start1(arr)
+    @l = ""
+    @pairs = []
+    convert_arr_to_pairs(arr)
+    correct = check(arr)
+    l "Correct parsing: #{correct.inspect}"
+    answer = 0
+    @pairs.each do |(a,b)|
+      numbers = b.map(&:size)
+      needed = numbers.select{|x| [2,4,3,7].include?(x)}
+      local_size = needed.size
+      answer += local_size
+      l "#{b.inspect} | #{numbers.inspect} | #{local_size} : #{needed.inspect} => #{answer}"
+    end
+    l_up "Answer is: #{answer}"
+    l_up "First 1."
+    return @l
+  end
+  def start2(arr)
+    @l = ""
+    l_up "Second 2."
+    return @l
+  end
+  def l(str)
+    @l += str + "\n"
+  end
+  def l_up(str)
+    @l = str + "\n" + @l
+  end
+  def convert_arr_to_pairs(arr)
+    @pairs = []
+    arr.each do |str|
+      str = str.strip
+      left,right = str.split(?|)
+      left = left.split(" ").map(&:strip)
+      right = right.split(" ").map(&:strip)
+      @pairs.push([left,right])
+    end
+  end
+  def check(arr)
+    arr.map(&:strip) == @pairs.map{|(a,b)| a.join(" ") + " | " + b.join(" ")}
+  end
+end
+
